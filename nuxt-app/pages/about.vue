fa34bc825e6d247fb06d82b3f91d5ae4dd4cc503
madjo7 <markodjo7@gmail.com>
2024-04-22 09:08:14 +0200

    first commit

---
+++ nuxt-app/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis
+      beatae dignissimos rerum voluptatibus a illum consectetur atque nihil?
+      Animi aspernatur ex architecto veniam temporibus odit maxime facere ipsam
+      consectetur.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ nuxt-app/pages/products/index.vue
@@ -0,0 +1,11 @@
+<!-- EMMETT: vbase-3-setup -->
+<!-- /products -->
+<template>
+  <div>
+    <p>asdasdsa</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
